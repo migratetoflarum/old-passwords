58afb7d3a95152ee64c9436f4e1546e81c76eeb8
David Sevilla Martín <dsevilla192@icloud.com>
2018-09-25 16:02:31 -0400

    Update for Flarum beta 8

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,8 @@ namespace MigrateToFlarum\OldPasswords;
 
 use Illuminate\Contracts\Events\Dispatcher;
 
-return function (Dispatcher $events) {
-    $events->subscribe(Listeners\CheckPassword::class);
-};
+return [
+    function (Dispatcher $events) {
+        $events->subscribe(Listeners\CheckPassword::class);
+    }
+];
